5c616b1bf132e631e1eb06cfd506fbc36cba9b2b
mauricioholanda <mauriciofholanda@gmail.com>
2021-06-17 09:53:27 -0300

    refactor: change imports

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.{ts,tsx}",
+  ],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
-    ".+\\.(ts|tsx)$": "ts-jest",
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+    "\\.scss$": "identity-obj-proxy",
   },
 };
